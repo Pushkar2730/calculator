15edadd637f50f8980fcbd01661e3125d3a8cb0d
Pushkar2730 <guptapushkar2730@gmail.com>
2021-09-03 22:01:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+function sum(int a,int b){
+return a+b;
+}
+
+function subs(int a,int b){
+    return a-b;
+}
+
+function divide(int a,int b){
+    return a/b;
+
+}
+
+function multiply(int a int b){
+    return a*b;
+}
+

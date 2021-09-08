32dc17a31c44b3d0f3bf60f7ac2e0f96a800d49f
Pushkar2730 <guptapushkar2730@gmail.com>
2021-09-08 15:34:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,89 @@
-function sum(int a,int b){
-return a+b;
-}
+ function getHistory(){
+ return document.getElementById("historyResult").innerText;
+ }
 
-function subs(int a,int b){
-    return a-b;
-}
+ function printHistory(num){
+    return document.getElementById("historyResult").innerText=num;
+ }
 
-function divide(int a,int b){
-    return a/b;
+ function getOutput(){
+    return document.getElementById("outputResult").innerText;
+ }
 
-}
 
-function multiply(int a int b){
-    return a*b;
-}
+ function printOutput(num){
+    
+    return document.getElementById("outputResult").innerText=num;
+ }
 
+ var output="";
+ var prev="";
+
+ var number = document.getElementsByClassName("number");
+ for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		
+        prev+=this.id;
+        printHistory(prev);
+            output=eval(prev);
+			printOutput(output);
+           
+    })
+    }
+   
+   
+ var operator = document.getElementsByClassName("operator");	
+    for(var i=0;i<operator.length;i++){
+        operator[i].addEventListener('click',function(){
+            if(this.id=="="){
+                output=eval(prev);
+                prev=output;
+            }
+            else if(this.id=="clear"){
+                prev="";
+                output="";
+            }
+            else if(this.id=="backspace"){
+                prev=prev.substr(0,prev.length-1);
+                output=eval(prev);
+            }
+           else if(this.id=="%"){
+               var i;
+               for(i=prev.length-1;prev[i]!=NaN;i--){
+              
+               }
+               alert(i);
+              var result=prev.substr(i+1,prev.length)/100;
+                
+                prev.substr(i+1,prev.length)=result;
+                output=eval(prev);
+                
+            }
+          else if(this.id=="."){
+                prev+=this.id;
+                if(isNaN(prev[prev.length-2])){
+                    var result=prev.substr(0,prev.length-2);
+                    output=eval(result);
+                }
+                else{
+                    var result=prev.substr(0,prev.length-1);
+                    output=eval(result);
+                }
+            }
+
+            else{
+            
+                prev+=this.id;
+                if(isNaN(prev[prev.length-1])){
+                var result=prev.substr(0,prev.length-1);
+                output=eval(result);
+                }
+                // else{
+                //     output=eval(prev);
+                // }
+            }
+            printHistory(prev);
+            printOutput(output);
+            
+         })
+    }
